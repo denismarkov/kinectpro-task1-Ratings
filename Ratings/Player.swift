38d72ac31e7d143edfa20815805d32afe0092d26
Denis Markov <keynsianets@gmail.com>
2018-07-19 14:45:19 +0300

    Add .gitignore file Change author name and email

---
+++ Ratings/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  Ratings
+//
+//  Created by Denis Markov on 7/19/18.
+//  Copyright © 2018 Denis Markov. All rights reserved.
+//
+
+import Foundation
---
+++ Ratings/PlayerCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerCell.swift
+//  Ratings
+//
+//  Created by Denis Markov on 7/19/18.
+//  Copyright © 2018 Denis Markov. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
